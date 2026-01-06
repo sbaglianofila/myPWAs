40284ab0f48f81feff6c65e39443b082e6d47aed
Fabio <ventunonove@gmail.com>
2026-01-06 18:01:42 +0100

    PWA Yahtzee primo commit

---
+++ Yahtzee/sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'yahtzee-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://cdn.tailwindcss.com'
+];
