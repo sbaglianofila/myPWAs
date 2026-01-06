0deb32a2d81ea8fbf40af2d1ceba0a8ece7fa760
Fabio <ventunonove@gmail.com>
2026-01-06 18:17:22 +0100

    Modficato SW e aggiunto codice a Index.html per renderla PWA

--- Yahtzee/sw.js
+++ Yahtzee/sw.js
@@ -9,3 +9,31 @@ const urlsToCache = [
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://cdn.tailwindcss.com'
 ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
